dd84b60c59acf798791b76e4de1b30a83ab5c8ab
Bas <Bas>
2023-06-13 14:48:18 +0200

    add testing section to bops with 'test_import_opensim' and 'test_import_c3d_data'

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-import unittest
-def my_fun():
-        return 3
+import pandas as pd
 
-class test_bops(unittest.TestCase):
-       
-    def test_function(self):
-        # Test 1
-        self.assertEqual(my_fun(), 'a')
-        
-        
-        # Test 2
-        assert len([1, 2, 3]) == 3
-        
-        
-            
-        
-if __name__ == '__main__':
-    # Call the test function
-    unittest.main()
+print(type(pd.DataFrame()))
