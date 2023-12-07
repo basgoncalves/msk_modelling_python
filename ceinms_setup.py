9c7499a38354443465f4552c5f8e2f44e65bb7b1
basgoncalves <basilio.goncalves7@gmail.com>
2023-12-07 10:04:27 +0100

    add settings json

--- ceinms_setup.py
+++ ceinms_setup.py
@@ -1,3 +1,8 @@
 
 # to import bops need to deactivate some packages not intalled (offline mode, consider this when packaging)
 import bops 
+
+print(bops.get_project_folder())
+# print(bops.select_folder())
+
+bops.create_project_settings()
