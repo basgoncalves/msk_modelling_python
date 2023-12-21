8988ac197a1cc62d85b30f478971ee7d4c7b2d51
Bas <basilio.goncalves7@gmail.com>
2023-12-21 13:57:37 +0100

    main_ceinms: Refactor code and add subject class and function to modify subjectinfo.csv

--- main_ceinms.py
+++ main_ceinms.py
@@ -9,48 +9,44 @@ import pkg_resources
 from src.add_to_system_path import is_admin
 from src import download_dependencies
 
+# dowload required packages
 # download_dependencies.create_virtual_environment()
 # download_dependencies.create_requirements()
 # download_dependencies.install_requirements()
-exit()
-# # Get the file names in the src directory
-# src_dir = 'src'
-# file_names = os.listdir(src_dir)
-# # Import all the modules in the src directory
-# for file_name in file_names:
-#     if file_name.endswith('.py'):
-#         module_name = file_name[:-3]  # Remove the .py extension
-#         module = __import__(f'{src_dir}.{module_name}', fromlist=[module_name])
+
 
 from src import database_tools
-
-
-
-
-
+import pandas as pd
 
 def print_design():
-    print('designed by Dr Bas')
-    print('designed by Dr Bas')
-
-root = tk.Tk()
-
-canvas = tk.Canvas(root, height=700, width=900, bg='#263D54')
-canvas.pack()
-
-# Run gui
-root.mainloop()
-
-gui()
-test()
-
-class test_bops(unittest.TestCase):
+    print('copyright by Basilio Goncalves')
     
-    ##### TESTS WORKING ######
-    def test_print(self):
-        print_design()
+class subject:
+    def __init__(self, id, age, group, column2, column3, column4, column5, column6, column7, column8, column9, column10):
+        self.id = id
+        self.age = age
+        self.group = group
+        self.column2 = column2
+        self.column3 = column3
+        self.column4 = column4
+        self.column5 = column5
+        self.column6 = column6
+        self.column7 = column7
+        self.column8 = column8
+        self.column9 = column9
+        self.column10 = column10
 
-# Load the            
-  
-if __name__ == '__main__':
-    print_design()
+simulations_folder = r'C:\Git\msk_modelling_python\example_data\running\simulations'
+main_folder = os.path.dirname(simulations_folder)
+subjectcsv = os.path.join(main_folder, 'subjectinfo.csv')
+
+def add_s_to_subject_info_id(subjectcsv):
+    df = pd.read_csv(subjectcsv)
+    df['id'] = 's' + df['id'].astype(str).str.zfill(3)
+    df.to_csv(os.path.join(main_folder, subjectcsv), index=False)
+    
+    print('file saved at: ' + os.path.join(main_folder, subjectcsv))
+
+df = pd.read_csv(subjectcsv)
+for column in df.columns.tolist():
+    print(column)
