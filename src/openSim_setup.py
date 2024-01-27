0a468e393411475f990d3dde8ca1ef99e1fe33a2
basgoncalves <basilio.goncalves7@gmail.com>
2024-01-27 16:48:34 +0100

    ** change opensim setup to 3.3

--- src/openSim_setup.py
+++ src/openSim_setup.py
@@ -2,9 +2,9 @@
 import os
 import struct
 import sys
-os.add_dll_directory("C:/OpenSim 4.3/bin")
-os.add_dll_directory(r"C:\Users\Bas\AppData\Local\Programs\Python\Python311\Lib\site-packages\opensim")
-os.add_dll_directory('C:\OpenSim 4.3\sdk\Simbody')
+os.add_dll_directory("C:/OpenSim 3.3/bin")
+os.add_dll_directory(r"C:\Users\Bas\AppData\Local\Programs\Python\Python38\Lib\site-packages\opensim")
+os.add_dll_directory('C:\OpenSim 3.3\sdk\Simbody')
 
 print('python installed with ' + str(struct.calcsize("P") * 8) + ' bits')
 
