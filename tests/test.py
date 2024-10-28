2d09b49a5bdbf3e033f286370b171b7b3b201d8d
basgoncalves <basilio.goncalves7@gmail.com>
2024-10-28 12:09:17 +0100

    * add febio tests

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,6 @@
 import tkinter as tk
 import msk_modelling_python as msk
 
-file_path = r"C:\Users\Bas\Downloads\hip\PersMeshPersF\kinematics_009_runStraight1\hip_rotation_r.txt"
-msk.src.bops.plot_from_txt(file_path, title='Hip Rotation R')
 
-file_path = r"C:\Users\Bas\Downloads\hip\PersMeshPersF\kinematics_009_runStraight1\hip_flexion_r.txt"
-msk.src.bops.plot_from_txt(file_path, title='Hip Flexion R')
-
-file_path = r"C:\Users\Bas\Downloads\hip\PersMeshPersF\kinematics_009_runStraight1\hip_adduction_r.txt"
-msk.src.bops.plot_from_txt(file_path, title='Hip Adduction R')
-
-msk.src.bops.plt.show()
 
 
