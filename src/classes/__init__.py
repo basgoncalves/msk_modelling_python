2520ef48204bc6ffb812e370643ca8fe5d00c90e
basgoncalves <basilio.goncalves7@gmail.com>
2024-10-31 14:52:52 +0100

    * Update classes and basic setup

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -23,6 +23,7 @@ class cmd_function:
         self.func(*args, **kwargs)
 
 
+#%% OSIM DATA CLASSES
 class osimData:
     def __init__(self,path):
         self.path = path
@@ -41,20 +42,55 @@ class osimData:
                 elif file == 'jointLoads.sto':
                     self.jointLoads = msk.bp.import_sto_data(os.path.join(path, file))
         
-
-# settings class
 class Task:
-    def __init__(self, path):
-        self.path = path
-        self.folders = os.listdir(path)
+    # For each task, create a class that contains the osimData objects
+    # check example folder structure: C:\Project\Subject\Task\osimData
+    def __init__(self, taskPath):
+        self.path = taskPath
+        self.folders = os.listdir(taskPath)
         
         for folder in self.folders:
-            folderPath = os.path.join(path, folder)
+            folderPath = os.path.join(taskPath, folder)
             self.__dict__[folder] = msk.osimData(folderPath)
             
 
+class Subject:
+    # For each subject, create a class that contains the Task objects
+    # check example folder structure: C:\Project\Subject\Task\osimData
+    def __init__(self, path):
+        self.path = path
+        self.tasks = os.listdir(path)
+        
+        for task in self.tasks:
+            taskPath = os.path.join(path, task)
+            if os.path.isdir(taskPath):
+                self.__dict__[task] = msk.Task(taskPath)
+        
+
+
+class Project:
+    # For each project, create a class that contains the Subject objects
+    # check example folder structure: C:\Project\Subject\Task\osimData
+    def __init__(self, projectPath):
+        self.path = projectPath
+        self.dataPath = os.path.join(projectPath, 'Data')
+        self.subjects = []
+        
+        for subject in os.listdir(self.dataPath):
+            subjectPath = os.path.join(self.dataPath, subject)
+            if os.path.isdir(subjectPath):
+                self.__dict__[subject] = msk.Subject(subjectPath)    
+                self.subjects.append(subject)
     
-    
+
+def isTask(var):
+    return isinstance(var, msk.Task)
+
+def isSubject(var):
+    return isinstance(var, msk.Subject)
+
+def isProject(var):
+    return isinstance(var, msk.Project)
     
         
 # END
--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,29 @@ import tkinter as tk
 import msk_modelling_python as msk
 import os
 
+project_path = r"C:\Git\charizard"
+data_path = os.path.join(project_path, 'data')
+osimModel_path = os.path.join(project_path, 'osimModel')
+
+
+project = msk.Project(project_path)
+print(project.subjects)
+
+exit()
+
+# Athlete data = folders in the data folder
+athlethes = os.listdir(data_path)
+
+for ath in athlethes:
+    # create a task object for each task
+    ath_path = os.path.join(data_path, ath)
+    tasks = os.listdir(ath_path)
+    
+    for task in tasks:
+        task_path = os.path.join(ath_path, task)
+        msk.Task(task_path)
+
+
 task_path = r"C:\Git\python-envs\msk_modelling\Lib\site-packages\msk_modelling_python\example_data\db_movements\running"
 task = msk.Task(task_path)
 
