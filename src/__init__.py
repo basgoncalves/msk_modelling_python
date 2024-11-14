c124b87494448a1e06d1197cbd4005265e279e0a
basgoncalves <basilio.goncalves7@gmail.com>
2024-11-14 18:49:09 +0100

    src: update error print opensim import

--- src/__init__.py
+++ src/__init__.py
@@ -8,8 +8,10 @@ except:
         pass     
     print('=============================================================================================')
     print('could not import opensim')
-    print('Check if __init__.py has "." before packages (e.g. "from .simbody" instead of "from simbody")')
+    print('check if the opensim python package is installed in your python environment')
     pythonPath = os.path.dirname(sys.executable)
     initPath = os.path.join(pythonPath,'lib\site-packages\opensim\__init__.py')
     print('init path is: ', initPath)    
+    print('For opensim installation, visit: https://simtk-confluence.stanford.edu/display/OpenSim/Scripting+with+Python')
     print('=============================================================================================\n\n\n\n\n')
+    
