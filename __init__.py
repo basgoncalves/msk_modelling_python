ab3fd538ce603c60cc51f41e1fa5ef9bb9dcb30e
basgoncalves <basilio.goncalves7@gmail.com>
2024-11-15 16:41:37 +0100

    ** move packages including ui to src

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from msk_modelling_python.src.bops import bops
 from msk_modelling_python.src.bops import ceinms
 from msk_modelling_python.src.utils import general_utils as ut
 import msk_modelling_python.src.plot as plot
-from msk_modelling_python import ui # import ui modules (not finished yet...)
+from msk_modelling_python.src import ui # import ui modules (not finished yet...)
 
 __version__ = '0.1.7'
 __testing__ = False
--- main.py
+++ main.py
@@ -6,9 +6,14 @@ Change settings on the .\settings.json file to change the behaviour of the modul
 import msk_modelling_python as msk
 
 if __name__ == "__main__":
-  msk.run_bops()
-  msk.test_msk()
-  msk.log_error('Tests passed for msk_modelling_python package')
+  try:
+    msk.run_bops()
+    msk.test_msk()
+    msk.log_error('Tests passed for msk_modelling_python package')
+  except Exception as e:
+    print("Error: ", e)
+    msk.log_error(e)
+    msk.bops.Platypus().sad()
 # # END
 
 
