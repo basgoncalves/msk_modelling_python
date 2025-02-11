fd853c1b5834b501131e176fcf3b94ed1e709e93
basgoncalves <basilio.goncalves7@gmail.com>
2025-02-11 22:16:57 +0100

    add distance from the trimesh

--- src/mri/hip_project/distance_between_meshs_from_stl.py
+++ src/mri/hip_project/distance_between_meshs_from_stl.py
@@ -2,7 +2,11 @@ import os
 import trimesh
 import numpy as np
 from matplotlib import pyplot as plt
-# may need to pip install "pyglet<2" and "rtree" to run this example
+from matplotlib import colors
+from trimesh import proximity
+import pandas as pd
+# import Seaborn as sns 
+# may need to pip install "pyglet<2", "rtree", "open3d" to run this example
 
 class Mesh():
     def __init__(self, path = None):
@@ -25,18 +29,42 @@ class Mesh():
     def split_mesh(self, n_chunks):
         chunk_size = len(self.vertices) // n_chunks
         mesh_list = []
-        
         # Split the vertices into chunks
         for i in range(0, len(self.vertices), chunk_size):
+
+            # Check if the chunk size is bigger than the remaining vertices
+            if i+chunk_size > len(self.vertices):
+                chunk_size = len(self.vertices) - i
             
             # Create a new mesh with the chunk
-            new_mesh = Mesh()
-            import pdb; pdb.set_trace()
-            new_mesh.Trimesh = trimesh.Trimesh(vertices=self.vertices[i:i+chunk_size], faces=self.Trimesh.faces)
-            mesh_list.append(new_mesh)
-        
-        return [self.vertices[i:i+chunk_size] for i in range(0, len(self.vertices), chunk_size)]
+            try:
+                new_mesh = Mesh()
+                chunk_vertices = self.vertices[i:i + chunk_size]
+                chunk_faces = self.Trimesh.faces[np.all(np.isin(self.Trimesh.faces, np.arange(i, i + chunk_size)), axis=1)]
+                chunk_faces -= i  # Adjust face indices to match the chunk vertices
+                new_mesh.Trimesh = trimesh.Trimesh(vertices=chunk_vertices, faces=chunk_faces)
+                new_mesh.vertices = new_mesh.Trimesh.vertices
+                mesh_list.append(new_mesh)
+            except Exception as e:
+                print(f'Error in chunk {i}')
+                print(e)
+                import pdb; pdb.set_trace()   
+                
+        return mesh_list
     
+    def add_to_current_plot(self, color='b'):
+        
+        # check if there is a current figure
+        if plt.fignum_exists(1) == False:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+        else:
+            fig = plt.gcf()
+        
+        # activate the current figure
+        ax = plt.gca()
+        ax.scatter(self.vertices[:, 0], self.vertices[:, 1], self.vertices[:, 2], c=color)
+        
     def distance_to_point(self, mesh):
         return np.linalg.norm(self.centroid() - mesh.centroid())
 
@@ -46,169 +74,73 @@ def print_loading_bar(current, total):
     block = int(round(bar_length * current / total))
     bar = "#" * block + "-" * (bar_length - block)
     print(f'Loading: [{bar}] {percentage:.2f}% ({current}/{total})')
+
+def save_3d_plot(fig, path):
+    if os.path.exists(path):
+        answer = input(f"File {path} already exists. Press Enter to overwrite (N to cancel)").lower()
+        if answer == 'n':
+            return
+    # save front view
+    fig.savefig(path)
     
+    # save side view
+    ax.view_init(elev=0, azim=90)
+    fig.savefig(path.replace('.png', '_side.png'))
+    
+    # save top view
+    ax.view_init(elev=90, azim=0)
+    fig.savefig(path.replace('.png', '_top.png'))
+    
+    # save isometric view
+    ax.view_init(elev=30, azim=30)
+    fig.savefig(path.replace('.png', '_iso.png'))
+    
+
 # Replace for the paths of the pelvis and femur meshes
-pelvis_path = r'c:\Users\Bas\ucloud\MRI_segmentation_BG\acetabular_coverage\048\Meshlab_BG\coverage_stick_method\Segmentation_r_pelvis.stl'
-femur_path = r'c:\Users\Bas\ucloud\MRI_segmentation_BG\acetabular_coverage\048\Meshlab_BG\coverage_stick_method\Segmentation_r_femur.stl'
+pelvis_path = r'c:\Users\Bas\ucloud\MRI_segmentation_BG\acetabular_coverage\048\Meshlab_BG\coverage_stick_method\acetabulum_l.stl'
+femur_path = r'c:\Users\Bas\ucloud\MRI_segmentation_BG\acetabular_coverage\048\Meshlab_BG\coverage_stick_method\femoral_head_l.stl'
 
 n_chunks = 10
-threshould = 90
+threshold_centroids = 90 
 
 # Paths to save the figures
 current_path = os.path.dirname(os.path.abspath(__file__))
 figures_path = os.path.join(current_path, 'figures')
+if os.path.exists(figures_path) == False:
+    os.mkdir(figures_path)
 
 # Load the meshes
 pelvis = Mesh(pelvis_path)
 femur = Mesh(femur_path)
 
-# Calculate the distance between the meshes using the signed distance function from trimesh
-print('Calculating distance between meshes ...')
+pelvis = trimesh.load(pelvis_path)
+femur = trimesh.load(femur_path)
 
+distance = proximity.signed_distance(pelvis, femur.vertices)
 
-pelvis_chunks = pelvis.split_mesh(n_chunks)
-import pdb; pdb.set_trace()
-# Print the number of vertices of each mesh
-print(f'pelvis vertices: {pelvis.vertices.shape}')
-print(f'femur vertices: {femur.vertices.shape}')
+# calculate the distance between the meshes
+distance_femur = pelvis.nearest.on_surface(femur.vertices)
+distance_pelvis = femur.nearest.on_surface(pelvis.vertices)
 
-# Flatten the vertices to calculate the distances
-flattened_pelvis = pelvis.vertices.reshape(-1, 3)
-flattened_femur = femur.vertices.reshape(-1, 3)
+# make all distances smaller than the threshold red
+distance_femur[2][distance_femur[2] < threshold_centroids] = 1
+distance_pelvis[2][distance_pelvis[2] < threshold_centroids] = 1
 
-# Split Pelvis vertices into chunks
-chunk_size = len(flattened_pelvis) // n_chunks
-chunk_list_pelvis = [flattened_pelvis[i:i+chunk_size] for i in range(0, len(flattened_pelvis), chunk_size)]
-
-# Split Femur vertices into chunks
-chunk_size = len(flattened_femur) // n_chunks
-chunk_list_femur = [flattened_femur[i:i+chunk_size] for i in range(0, len(flattened_femur), chunk_size)]
-
-# Calculate the mean of each chunk which would be equivalent to the centroid of the chunk (not the mesh)
-chunks_femur_centroids = [np.mean(chunk_femur, axis=0) for chunk_femur in chunk_list_femur]
-chunk_pelvis_centroids = [np.mean(chunk_pelvis, axis=0) for chunk_pelvis in chunk_list_pelvis]
-
-# Calculate the distances between the chunks
-distances_grid = np.zeros((chunk_size, chunk_size))
-distance_between_chunks = []
-
-if len(chunks_femur_centroids) != len(chunk_pelvis_centroids):
-    raise ValueError("The number of chunks is different between the femur and pelvis")
-
-# Loop through the chunks to calculate the distances between the centroids of the chunks
-for i_femur, femur_chunk in enumerate(chunks_femur_centroids):
-    print_loading_bar(i_femur, n_chunks)
-    mean_distance = []
-    for i_pelvis, pelvis_chunk in enumerate(chunk_pelvis_centroids):
-        
-        # Calculate the distance between the centroids of the chunks
-        distance = np.linalg.norm(femur_chunk - pelvis_chunk)
-        
-        # Save the distance in the grid
-        distances_grid[i_femur, i_pelvis] = np.linalg.norm(femur_chunk - pelvis_chunk)
-        
-        # Save the mean distance between the chunks
-        mean_distance.append(distance)
-        
-    distance_between_chunks.append(min(mean_distance))
-    
-    if distance_between_chunks[-1] > threshould:
-        import pdb; pdb.set_trace()
-        print(f'Chunk {i_femur} is above the threshold')
-        
-    else:
-        pelvis.show_3d_mesh()
-        import pdb; pdb.set_trace()
-        print(f'Chunk {i_femur} is below the threshold')
-    
-
-
+# make all distances bigger than the threshold gray
+distance_femur[2][distance_femur[2] >= threshold_centroids] = 0
+distance_pelvis[2][distance_pelvis[2] >= threshold_centroids] = 0
 
 import pdb; pdb.set_trace()
+# plot the distance
+fig = plt.figure()
+ax = plt.subplot(111, projection='3d')
+ax.scatter(femur.vertices[:,0], femur.vertices[:,1], femur.vertices[:,2],c=distance_femur[2], cmap='Reds')
+ax.scatter(pelvis.vertices[:,0], pelvis.vertices[:,1], pelvis.vertices[:,2],c=distance_pelvis[2])
+# add colorbar
+norm = colors.Normalize(vmin=distance.min(), vmax=distance.max())
+sm = plt.cm.ScalarMappable(cmap='Reds', norm=norm)
 
-for i, (chunk_pelvis, chunk_femur) in enumerate(zip(chunk_list_pelvis, chunk_list_femur)):
-    print_loading_bar(i, n_chunks)
-    
-    mean_femur_chuck = np.mean(chunk_femur, axis=0)
-    mean_pelvis_chuck = np.mean(chunk_pelvis, axis=0)
-    mean_distance = np.linalg.norm(mean_femur_chuck - mean_pelvis_chuck)
-    ax = plt.figure().add_subplot(111, projection='3d')
-    ax.scatter(chunk_pelvis[:, 0], chunk_pelvis[:, 1], chunk_pelvis[:,2], c='b')
-    ax.scatter(chunk_femur[:, 0], chunk_femur[:, 1], chunk_femur[:,2], c='r')
+plt.show()
 
-    ax.scatter(mean_pelvis_chuck[0], mean_pelvis_chuck[1], mean_pelvis_chuck[2], c='g', s=100)
-    ax.scatter(mean_femur_chuck[0], mean_femur_chuck[1], mean_femur_chuck[2], c='y', s=100)
-
-    fig_path = os.path.join(os.path.dirname(pelvis_path), f'chunk_{i}.png')
-    plt.savefig(fig_path)
-
-    import pdb; pdb.set_trace()
-    
-chunk_pelvis = chunk_list_pelvis[0]
-chunk_femur = chunk_list_femur[0]
-
-
-
-
-
-
-for chunk_pelvis, chunk_femur in zip(chunk_list_pelvis, chunk_list_femur):
-    print(f'Processing chunk {chunk_id}')
-    chunk_id += 1
-    min_distances = np.full(len(chunk_pelvis), np.inf)
-    for i in range(0, len(chunk_femur), chunk_size):
-        chunk_femur = flattened_femur[i:i+chunk_size]
-        dist_matrix = np.linalg.norm(chunk_pelvis[:, None] - chunk_femur, axis=2)
-        min_distances = np.minimum(min_distances, dist_matrix.min(axis=1))
-        distances[i:i+chunk_size] = min_distances
-
-for i in range(0, len(flattened_pelvis), chunk_size):
-    print_loading_bar(i, len(flattened_pelvis))
-    chunk_pelvis = flattened_pelvis[i:i+chunk_size]
-    chunk_femur = flattened_femur[i:i+chunk_size]
-
-    # plot the chunk
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(chunk_pelvis[:, 0], chunk_pelvis[:, 1], chunk_pelvis[:, 2], color='b')
-    ax.scatter(chunk_pelvis[0, 0], chunk_pelvis[0, 1], chunk_pelvis[0, 2], color='r', s=100)
-    ax.scatter(chunk_pelvis[-1, 0], chunk_pelvis[-1, 1], chunk_pelvis[-1, 2], color='g', s=100)
-    ax.scatter(chunk_pelvis[500, 0], chunk_pelvis[500, 1], chunk_pelvis[500, 2], color='y', s=100)
-    
-    ax.scatter(chunk_femur[:, 0], chunk_femur[:, 1], chunk_femur[:, 2], color='r')
-    ax.scatter(chunk_femur[0, 0], chunk_femur[0, 1], chunk_femur[0, 2], color='b', s=100)
-    ax.scatter(chunk_femur[-1, 0], chunk_femur[-1, 1], chunk_femur[-1, 2], color='g', s=100)
-    ax.scatter(chunk_femur[500, 0], chunk_femur[500, 1], chunk_femur[500, 2], color='y', s=100)
-    plt.show()
-    
-    # make first element red
-    
-    
-    
-    
-import pdb; pdb.set_trace()
-exit()
-
-
-flattened_pelvis = pelvis.vertices.reshape(-1, 3)
-flattened_femur = femur.vertices.reshape(-1, 3)
-
-# Calculate the distances in chunks to avoid memory issues
-chunk_size = 1000
-distances = np.zeros(len(flattened_pelvis))
-
-for i in range(0, len(flattened_pelvis), chunk_size):
-    print_loading_bar(i, len(flattened_pelvis))
-    chunk_pelvis = flattened_pelvis[i:i+chunk_size]
-    min_distances = np.full(len(chunk_pelvis), np.inf)
-    for j in range(0, len(flattened_femur), chunk_size):
-        chunk_femur = flattened_femur[j:j+chunk_size]
-        dist_matrix = np.linalg.norm(chunk_pelvis[:, None] - chunk_femur, axis=2)
-        min_distances = np.minimum(min_distances, dist_matrix.min(axis=1))
-    distances[i:i+chunk_size] = min_distances
-
-# Plot the pelvis with color based on the distance to the femur
-pelvis.Trimesh.visual.vertex_colors = trimesh.visual.interpolate(distances, color_map='viridis')
-pelvis.show_3d_mesh()
-    
-
+distance_save_path = os.path.join(current_path, 'distance.csv')
+pd.DataFrame(distance[2]).to_csv(distance_save_path)
