1e1b937dc7644e7f072e18b480dcd2351fef8115
basgoncalves <basilio.goncalves7@gmail.com>
2025-02-24 21:57:40 +0100

    add quick test notebook and update quick_test.py for VTP file handling

--- src/tests/quick_test.py
+++ src/tests/quick_test.py
@@ -9,28 +9,14 @@ import matplotlib.pyplot as plt
 parent_dir = os.path.dirname(__file__)
 start_time = time.time()
 ################
-file = r'C:\Git\research_data\Projects\runbops_FAIS_phd\results\mean_ci\mean_ci.csv'
-# df =  msk.bops.pd.read_csv(file)
 
-# stats = file.replace('mean_ci.csv', 'group_comparison.xlsx')
-# df_stats = pd.read_excel(stats)
-
-# # radar plot 
-# line_columns = ['mean', 'ci_low', 'ci_high']
+initial_path =  r'C:\Git\research_data\models\Gerometry_bones\femur'
+vtp_path = msk.ui.select_file("Select the VTP file to load", staring_path=initial_path)
 
 
-# con = df[df['group'] == 'CON']
-# fais = df[df['group'] == 'FAIS']
-# cam = df[df['group'] == 'CAM']
-
-# column_dict = ['Alpha angle' 'Centre edge angle' 'Coverage_tested_leg_BG'
-#  'sphere_coverage_tested_leg_bg' 'HJCF_BW' 'HJCF_BW_normalised'
-#  'HJCF_Newtons' 'HJCF_Newtons_normalised' 'HJCF_pHF_Newtons'
-#  'HJCF_pHF_Newtons_normalised' 'HJCF_pHF_BW' 'HJCF_pHF_BW_normalised'
-#  'Weight' 'Height' 'Age']
 
 
-# print(df.head())
-# print(df.columns)
-# print(df['column'].unique())
 
+
+################
+print(f"--- {time.time() - start_time} seconds ---")
