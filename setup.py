d9353d8603c41cdac10388eca10d8fbd7228e724
basgoncalves <basilio.goncalves7@gmail.com>
2025-04-14 20:50:42 +0200

    feat: enhance package metadata and update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     version=__version__,
     author="Bas",
     author_email="basilio.goncalves7@gmail.com",
-    description="A Python package for musculoskeletal modelling",
-    long_description=long_description,
+    description=f"A Python package for musculoskeletal modelling (version {__version__})",
+    long_description=f"{long_description}\n\nVersion: {__version__}",
     long_description_content_type="text/markdown",
     url="https://github.com/your_username/msk_modelling_python",
     packages=find_packages(),
@@ -21,10 +21,12 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "numpy",
-        "numpy-stl",
-        "pandas",
-        "c3d"
+        "numpy>=1.21.0",
+        "numpy-stl>=2.16.0",
+        "pandas>=1.3.0",
+        "c3d>=0.3.0",
+        "customtkinter>=5.2.0",
+        "tkinter>=8.6.0",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
