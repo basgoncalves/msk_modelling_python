989836cf8b4dbffa757eb52a21204ce53ba9fa07
basgoncalves <basilio.goncalves7@gmail.com>
2025-04-15 14:51:10 +0200

    Add OpenSim import handling and update version to 0.0.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.0.6'  # Define the version here
+__version__ = '0.0.7'  # Define the version here
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
