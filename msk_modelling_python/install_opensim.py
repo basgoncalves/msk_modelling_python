2fb6d74af7a7122ff3c57976be2fa799657024b4
basgoncalves <basilio.goncalves7@gmail.com>
2025-04-24 19:49:35 +0200

    Refactor install_opensim.py: Use sys.executable for command execution and improve command logging

--- msk_modelling_python/install_opensim.py
+++ msk_modelling_python/install_opensim.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import sys
 
 def run(osim_version='4.5'):
     try:
@@ -7,22 +8,31 @@ def run(osim_version='4.5'):
         opensim_sdk_path = rf'C:\OpenSim {osim_version}\sdk\Python'
         if not os.path.exists(opensim_sdk_path):
             raise FileNotFoundError(f"Path does not exist: {opensim_sdk_path}")
-        
-    
+
+
         os.chdir(opensim_sdk_path)
         print(f"Changed directory to: {opensim_sdk_path}")
+
+        # Print the current working directory
+        print(f"Current working directory: {os.getcwd()}")
 
         # Run the setup script for Windows Python 3.8
         print("Running setup script for Windows Python 3.8...")
         setup_script = 'setup_win_python38.py'
-        command = 'python ' + setup_script
-        subprocess.run(command, check=True)
+        
+        command_list = [sys.executable, setup_script]
+        print(f"Executing:")
+        print(f"{' '.join(command_list)}") # Print the command as it would look in the shell
+        
+        subprocess.run(command_list, check=True, cwd=opensim_sdk_path) # Use list and specify cwd explicitly
         print(f"Executed: python {setup_script}")
 
         # Install the Python bindings
         print("Installing OpenSim Python bindings...")
-        install_command = ['python', '-m', 'pip', 'install', '.']
-        subprocess.run(install_command, check=True)
+        install_command_list = [sys.executable, '-m', 'pip', 'install', '.']
+        print(f"Executing:")
+        print(f"{' '.join(install_command_list)}") # Print the command
+        subprocess.run(install_command_list, check=True, cwd=opensim_sdk_path) # Use list and specify cwd explicitly
         print("Executed: python -m pip install .")
 
         print("OpenSim Python bindings installation process completed successfully.")
